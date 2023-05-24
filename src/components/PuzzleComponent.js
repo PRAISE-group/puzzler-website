f62c85552772a3e2dccaf631449c9cfefde87436
lahiri-phdworks <lahiri.phdworks@gmail.com>
2023-05-24 22:26:08 +0530

    [WIP] Puzzler Layout -- firebase integration

--- src/components/PuzzleComponent.js
+++ src/components/PuzzleComponent.js
@@ -27,10 +27,11 @@ const PuzzleOptionsImageComponent = (props) => {
             <input
                 type="checkbox"
                 id={optionsId}
-                value=""
                 class="hidden peer"
-                required=""
-                onClick={(e) => props.setOptionName(`${props.optionName}`)}
+                onClick={(e) => {
+                    if (e.target.checked) props.setOptionName(`${props.optionName}`);
+                    else props.setOptionName('');
+                }}
             />
             <label
                 for={optionsId}
@@ -115,6 +116,7 @@ const PuzzleComponent = (props) => {
 
     async function handleSubmit(event, loginId, puzzleId, option, rating) {
         event.preventDefault();
+        if (option === '' || rating === 0) alert('Please choose option and rating!');
         await effects.submitPuzzleRating(loginId, puzzleId, rating);
         await effects.submitPuzzleOption(loginId, puzzleId, option);
     }
@@ -299,7 +301,7 @@ const PuzzleComponent = (props) => {
                         handleSubmit(e, 'pz34564', props.name, optionName, rating);
                     }}
                     class="text-white bg-blue-700 mx-auto hover:bg-blue-800 
-                    focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer
+                    focus:ring-4 focus:outline focus:ring-blue-300 cursor-pointer
                     font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     Submit option and rating
